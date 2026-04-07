46bee5810250bedc1861cdc594d6bdf3d00cf622
keshav-provus <keshav@provus.ai>
2026-04-07 21:28:38 +0530

    refactor: move bar chart width calculations from template inline styles to renderedCallback DOM manipulation

--- force-app/main/default/lwc/cpqQuoteSummary/cpqQuoteSummary.js
+++ force-app/main/default/lwc/cpqQuoteSummary/cpqQuoteSummary.js
@@ -27,19 +27,16 @@ export default class CpqQuoteSummary extends LightningElement {
         return this.laborTotal + this.productTotal + this.addonTotal;
     }
 
-    get laborBarStyle() {
-        const pct = this.grandTotal > 0 ? (this.laborTotal / this.grandTotal) * 100 : 0;
-        return `width: ${pct}%;`;
+    get laborWidth() {
+        return this.grandTotal > 0 ? (this.laborTotal / this.grandTotal) * 100 : 0;
     }
 
-    get productBarStyle() {
-        const pct = this.grandTotal > 0 ? (this.productTotal / this.grandTotal) * 100 : 0;
-        return `width: ${pct}%;`;
+    get productWidth() {
+        return this.grandTotal > 0 ? (this.productTotal / this.grandTotal) * 100 : 0;
     }
 
-    get addonBarStyle() {
-        const pct = this.grandTotal > 0 ? (this.addonTotal / this.grandTotal) * 100 : 0;
-        return `width: ${pct}%;`;
+    get addonWidth() {
+        return this.grandTotal > 0 ? (this.addonTotal / this.grandTotal) * 100 : 0;
     }
 
     get phaseBreakdown() {
@@ -54,7 +51,27 @@ export default class CpqQuoteSummary extends LightningElement {
         return Object.keys(phases).map(name => ({
             name,
             total: phases[name],
-            widthStyle: `width: ${(phases[name] / maxTotal) * 100}%;`
+            pct: (phases[name] / maxTotal) * 100
         }));
+    }
+
+    renderedCallback() {
+        // Set Item Type breakdown bar widths
+        const laborBar = this.template.querySelector('.bar.labor');
+        const productBar = this.template.querySelector('.bar.products');
+        const addonBar = this.template.querySelector('.bar.addons');
+
+        if (laborBar) laborBar.style.width = `${this.laborWidth}%`;
+        if (productBar) productBar.style.width = `${this.productWidth}%`;
+        if (addonBar) addonBar.style.width = `${this.addonWidth}%`;
+
+        // Set Phase Breakdown progress widths
+        const phaseRows = this.template.querySelectorAll('.phase-row');
+        phaseRows.forEach((row, index) => {
+            const progressFill = row.querySelector('.progress-fill');
+            if (progressFill && this.phaseBreakdown[index]) {
+                progressFill.style.width = `${this.phaseBreakdown[index].pct}%`;
+            }
+        });
     }
 }
